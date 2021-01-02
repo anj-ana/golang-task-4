461e38c451bba2411352ca85657ac12908f4b20a
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-01-02 06:26:05 -0800

    Add files via upload

---
+++ array.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var food [10][10]string
+	var row, col int
+	fmt.Printf("Enter no of rows:\n")
+	fmt.Scanf("%d\n", &row)
+	fmt.Printf("Enter no of columns :\n")
+	fmt.Scanf("%d\n", &col)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			fmt.Printf(" food %d %d :\t", i+1, j+1)
+			fmt.Scanf("%s\t", &food[i][j])
+		}
+	}
+	fmt.Printf("\t")
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			fmt.Printf("%s\t", food[i][j])
+			if j == col-1 {
+				fmt.Printf(" \t ")
+
+			}
+		}
+	}
+
+}
